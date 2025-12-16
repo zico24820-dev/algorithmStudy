7a4cc4670c19f98eee1ef7a443d5e0730159170e
seonggyun <zico24820@gmail.com>
2025-12-16 20:00:13 +0900

    [Bronze V] Title: A/B, Time: 196 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       double b = sc.nextInt();
+    
+       double result = a/b;
+       
+       System.out.println(result);
+   }
+}
